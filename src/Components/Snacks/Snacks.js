87840812ef89932e93150ca6e917ef3ef582e937
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-17 10:15:46 -0600

    Add componentDidMount with fetch for snack ids

--- src/Components/Snacks/Snacks.js
+++ src/Components/Snacks/Snacks.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import './Snacks.css';
 import BrowseSnacks from '../BrowseSnacks/BrowseSnacks'
+import { getAllSnacks } from '../../Helpers/apiCalls'
 
-class Snack extends Component {
+class Snacks extends Component {
   constructor() {
     super();
     this.state = {
-      allSnackDetails: [],
+      // allSnackDetails: [],
       allSnackIds: [],
       allSnackPrices: [],
-      recurringSnacks: []
+      recurringSnacks: [],
+      error: '',
     }
+  }
+
+  componentDidMount() {
+    getAllSnacks()
+      .then(randomSnackIds => {
+        this.setState({ allSnackIds: randomSnackIds })
+      })
+   
+    // getAllSnacks()
+    //   .then((snacks) => {
+    //     const fetchedSnackIds = snacks.map((snack) => {
+    //       return snack.products.sku
+    //     })
+    //     this.setState({ allSnackIds: fetchedSnackIds, error: '' });
+    //   })
+    //   .catch((err) => {
+    //     this.setState({ error: "We're sorry, something went wrong. Please try again." });
+    //   });
   }
 
   render() {
@@ -20,4 +40,4 @@ class Snack extends Component {
   };
 }
 
-export default Snack;
+export default Snacks;
