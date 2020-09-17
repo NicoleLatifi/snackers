fe4fa8740492612a22b3c47c75d6f0c94490d324
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-17 15:56:57 -0600

    Add function that fetches snack details

--- src/Helpers/apiCalls.js
+++ src/Helpers/apiCalls.js
@@ -1,4 +1,4 @@
-export const getAllSnacks = () => {
+export const getAllSnacksIds = () => {
   return fetch(`https://api.wegmans.io/products/categories/6942-629-648?api-version=2018-10-18&subscription-key=c455d00cb0f64e238a5282d75921f27e`)
     .then((response) => response.json())
     .then((data) => {
@@ -7,8 +7,18 @@ export const getAllSnacks = () => {
         const numberFetched = data.products.length;
         const randomIndex = Math.floor(Math.random() * Math.floor(numberFetched))
         randomSnackIds.push(data.products[randomIndex].sku)
-        console.log(randomSnackIds)
       }
       return randomSnackIds
     })
 };
+
+export const getSnackDetail = (snackId) => {
+  return fetch(`https://api.wegmans.io/products/${snackId}?api-version=2018-10-18&Subscription-Key=c455d00cb0f64e238a5282d75921f27e`)
+    .then((response) => response.json())
+    .then((data) => {
+      let fetchedDetail = {
+      };
+      fetchedDetail.name = data.name
+      return fetchedDetail
+    })
+}
