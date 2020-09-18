9fe6f7476891e5224ed2694631b144fa101ec82b
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-18 11:42:10 -0600

    Pass in recurringSnacks prop

--- src/Components/BrowseSnacks/BrowseSnacks.js
+++ src/Components/BrowseSnacks/BrowseSnacks.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './BrowseSnacks.css';
 import BrowsingSnackCard from '../BrowsingSnackCard/BrowsingSnackCard'
 
-const BrowseSnacks = ({ allSnacksDetails, allSnacksIds }) => {
+const BrowseSnacks = ({ allSnacksDetails, allSnacksIds, recurringSnacks }) => {
   return (
     allSnacksIds.map((snackId) => {
       return <BrowsingSnackCard
       allSnacksDetails={allSnacksDetails}
       snackId={snackId}
+      recurringSnacks={recurringSnacks}
       key={snackId}
       />
     })
