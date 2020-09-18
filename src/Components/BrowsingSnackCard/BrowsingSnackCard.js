c7d66e55dededce15f982e6158e5c4f73f65e84b
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-18 11:45:21 -0600

    Render snack details and Add button

--- src/Components/BrowsingSnackCard/BrowsingSnackCard.js
+++ src/Components/BrowsingSnackCard/BrowsingSnackCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './BrowsingSnackCard.css';
+import Button from '../Button/Button'
 
-const BrowsingSnackCard = () => {
+const BrowsingSnackCard = ({ allSnacksDetails, snackId, recurringSnacks }) => {
+  const snackDetails = allSnacksDetails[snackId]
   return (
   <>
     <h2>Snack Name</h2>
+    <img className='snack-image' src={snackDetails.image} alt={snackDetails.name} />
+    <p>{snackDetails.name}</p>
+    <p>{snackDetails.brand}</p>
+    <p>${snackDetails.price} / {snackDetails.sizeValue} {snackDetails.sizeUnit}</p>
+    <p>{snackDetails.allergens}</p>
+    <p>{snackDetails.organic}</p>
+
+    { !Object.keys(recurringSnacks).includes(snackId) &&
+      <Button buttonText="Add" /> }
   </>  
   );
 }
