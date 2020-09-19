80e8a09b0fe16b74001be2cf52726b51897690f9
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-19 03:09:47 -0600

    Make button render dynamically

--- src/Components/QuantityButton/QuantityButton.js
+++ src/Components/QuantityButton/QuantityButton.js
@@ -5,23 +5,33 @@ class Button extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      buttonText: ''
+    }
+  }
+
+  componentDidMount = () => {
+    if (this.props.buttonType === "Decrease") {
+      this.setState({ buttonText: "-" })
+    } else {
+      this.setState({ buttonText: "+"})
     }
   }
 
   handleClick = () => {
     if (this.props.buttonType === "Decrease") {
       this.props.decreaseRecurringQuantity(this.props.snackId)
+      console.log("aaa")
+    }
+    if (this.props.buttonType === "Increase") {
+      this.props.increaseRecurringQuantity(this.props.snackId)
+      console.log("bbb")
     }
   }
 
   render() {
     return (
-      <div className="quantity-buttons-container">
-        <button aria-label="Decrease quantity" onClick={this.handleClick} >-</button>
-        <span>{this.props.recurringSnacks[this.props.snackId].quantity}</span>
-        <button aria-label="Decrease quantity" onClick={this.handleClick} >+</button>
-      </div>
-      )
+      <button aria-label={`${this.props.buttonType} quantity`} onClick={this.handleClick} >{this.state.buttonText}</button>
+    )
   }
 }
 
