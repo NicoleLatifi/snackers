d101d1976e9cc5403c01c245aceca8eb1fb55109
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-20 13:50:16 -0600

    Change p to h3 and remove commented out code

--- src/Components/RecurringSnackCard/RecurringSnackCard.js
+++ src/Components/RecurringSnackCard/RecurringSnackCard.js
@@ -5,16 +5,13 @@ import QuantityButton from '../QuantityButton/QuantityButton'
 
 const RecurringSnackCard = ({ allSnacksDetails, snackId, addSnack, decreaseRecurringQuantity, increaseRecurringQuantity, removeFromRecurring, pauseRecurringSnack }) => {
   const snackDetails = allSnacksDetails[snackId]
-  
+
   return (
   <>
-    <h2>Snack Name</h2>
     <img className='snack-image' src={snackDetails.image} alt={snackDetails.name} />
-    <p>{snackDetails.name}</p>
+    <h3>{snackDetails.name}</h3>
     <p>{snackDetails.brand}</p>
     <p>${snackDetails.price} / {snackDetails.sizeValue} {snackDetails.sizeUnit}</p>
-    <p>{snackDetails.allergens}</p>
-    <p>{snackDetails.organic}</p>
 
     { allSnacksDetails[snackId].recurringStatus !== "active" &&
       <>
@@ -61,45 +58,3 @@ const RecurringSnackCard = ({ allSnacksDetails, snackId, addSnack, decreaseRecur
 }
 
 export default RecurringSnackCard;
-
-
-// { allSnacksDetails[snackId].quantity === 0 &&
-//   <>
-//     <Button
-//       buttonText="Reactivate" 
-//       snackId={snackId}
-//       addSnack={addSnack}
-//     />
-//     <Button
-//       buttonText="Remove" 
-//       snackId={snackId} 
-//       removeFromRecurring={removeFromRecurring}
-//     />
-//   </>
-// }
-
-// { allSnacksDetails[snackId].quantity > 0 &&
-//   <div className="quantity-buttons-container">
-//     <QuantityButton
-//       snackId={snackId}
-//       buttonText="-"
-//       buttonType="Decrease"
-//       fromRecurringPage="true"
-//       decreaseRecurringQuantity={decreaseRecurringQuantity}
-//       increaseRecurringQuantity={increaseRecurringQuantity}
-//     /> 
-//     <span>{allSnacksDetails[snackId].quantity}</span>
-//     <QuantityButton
-//       snackId={snackId}
-//       buttonText="+"
-//       buttonType="Increase"
-//       decreaseRecurringQuantity={decreaseRecurringQuantity}
-//       increaseRecurringQuantity={increaseRecurringQuantity}
-//     /> 
-//     <Button
-//     buttonText="Pause" 
-//     snackId={snackId}
-//     pauseRecurringSnack={pauseRecurringSnack}
-//     />
-//   </div>
-// }
