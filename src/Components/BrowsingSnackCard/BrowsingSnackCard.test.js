5ad614877e07589b58efc761709516cc918d9abb
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-20 17:22:27 -0600

    Test for correct content of cards

--- src/Components/BrowsingSnackCard/BrowsingSnackCard.test.js
+++ src/Components/BrowsingSnackCard/BrowsingSnackCard.test.js
@@ -1,33 +1,72 @@
 import React from 'react'
-import '@testing-library/jest-dom'
+// import '@testing-library/jest-dom'
 import { screen, render } from '@testing-library/react'
 import BrowsingSnackCard from './BrowsingSnackCard'
 
 describe('BrowsingSnackCard Component', () => {
-  it('Should render snack details and Add button', () => {
-    const allSnacksDetails = {
-      123: {
+  let allSnacksDetails
+  beforeEach(() => {
+    allSnacksDetails = {
+      101: {
         name: "Chips",
-        brand: "Wegmans",
+        brand: "Lays",
+        price: 1,
         sizeValue: 10,
         sizeUnit: "OZ",
-        allergens: [],
-        organic: false,
-        image: "https://wfmproducts.azureedge.net/images-500/00077890461808.jpg"
+        image: "https://wfmproducts.azureedge.net/images-500/00077890461808.jpg",
+        quantity: 0
+      },
+      202: {
+        name: "Popcorn",
+        quantity: 3,
+        recurringStatus: "active"
       }
     }
+  })
 
+  it('Should render snack details and Add button', () => {
+    const snackId = 101
+    
     render (
       <BrowsingSnackCard
         allSnacksDetails={allSnacksDetails} 
-        snackId='123'
+        snackId={snackId}
         recurringSnacks='[]'
         />
     )
 
-    const name = screen.getByText('Chips')
-    // test for more elements once styling is finalized
+    const name = screen.getByRole('heading', {name: 'Chips'})
+    const brand = screen.getByText('Lays')
+    const price = screen.getByText('$1 / 10 OZ')
+    const image = screen.getByRole('img', { name: 'Chips'})
+    const addButton = screen.getByRole('button', {name: 'Add snack'})
 
     expect(name).toBeInTheDocument
+    expect(brand).toBeInTheDocument
+    expect(price).toBeInTheDocument
+    expect(image).toBeInTheDocument
+    expect(addButton).toBeInTheDocument
+  })
+
+  it('Should display the correct content when rendering active snack', () => {
+    const snackId = 202
+
+    render (
+      <BrowsingSnackCard 
+        allSnacksDetails={allSnacksDetails}
+        snackId={snackId}
+        recurringSnacks='[202]'
+      />
+    )
+
+    const name = screen.getByRole('heading', { name: 'Popcorn'})
+    const decreaseButton = screen.getByRole('button', {name: 'Decrease quantity'})
+    let quantity = screen.getByText('3')
+    const increaseButton = screen.getByRole('button', {name: 'Increase quantity'})
+    
+    expect(name).toBeInTheDocument
+    expect(decreaseButton).toBeInTheDocument
+    expect(quantity).toBeInTheDocument
+    expect(increaseButton).toBeInTheDocument
   })
 })
