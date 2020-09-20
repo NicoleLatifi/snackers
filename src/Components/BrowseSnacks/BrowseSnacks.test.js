49d108136836efa4a4bc6c3294461ccbef03a1bc
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-20 17:36:33 -0600

    Test for the snack card to render

---
+++ src/Components/BrowseSnacks/BrowseSnacks.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import BrowseSnacks from './BrowseSnacks';
+import { screen, fireEvent, render } from '@testing-library/react';
+
+describe('Browse Component', () => {
+  let allSnacksDetails
+  beforeEach(() => {
+    allSnacksDetails = {
+      101: {
+        name: "Chips",
+        brand: "Wegmans",
+        sizeValue: 10,
+        sizeUnit: "OZ",
+        allergens: [],
+        organic: false,
+        image: "https://wfmproducts.azureedge.net/images-500/00077890461808.jpg",
+        quantity: 0
+      }
+    }
+  })
+
+  it('Should render snacks', () => {
+    const allSnacksIds = [101]
+
+    render (
+      <BrowseSnacks 
+        allSnacksDetails={allSnacksDetails}
+        allSnacksIds={allSnacksIds}
+      />
+    )
+
+    const name = screen.getByRole('heading', { name: 'Chips'})
+    const addButton = screen.getByRole('button', { name: 'Add snack' })
+
+    expect(name).toBeInTheDocument
+    expect(addButton).toBeInTheDocument
+  })
+})
