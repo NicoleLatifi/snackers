c9eb09a97f3a3ed29b0ada94f6b1d823ff235ad3
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-20 12:56:33 -0600

    Test for correct content when no recurring snacks and one recurring snack

---
+++ src/Components/RecurringSnacks/RecurringSnacks.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import RecurringSnacks from './RecurringSnacks';
+import { screen, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('BrowsingSnackCard Component', () => {
+  let allSnacksDetails
+  beforeEach(() => {
+    allSnacksDetails = {
+      101: {
+        name: "Chips",
+        brand: "Wegmans",
+        sizeValue: 10,
+        sizeUnit: "OZ",
+        allergens: [],
+        organic: false,
+        image: "https://wfmproducts.azureedge.net/images-500/00077890461808.jpg"
+      }
+    }
+  })
+
+  it('Should render message when there are no recurring snacks', () => {
+    const recurringSnacksIds = [];
+
+    render (
+      <RecurringSnacks 
+        allSnacksDetails={allSnacksDetails}
+        recurringSnacksIds={recurringSnacksIds}
+      />
+    )
+
+    const message = screen.getByRole('heading', { name: 'You have no recurring snacks.'})
+
+    expect(message).toBeInTheDocument
+  })
+
+  it('Should render any recurring snacks', () => {
+    const recurringSnacksIds = [101]
+
+    render (
+      <RecurringSnacks 
+        allSnacksDetails={allSnacksDetails}
+        recurringSnacksIds={recurringSnacksIds}
+      />
+    )
+
+    const name = screen.getByRole('heading', { name: 'Chips'})
+
+    expect(name).toBeInTheDocument
+  })
+})
