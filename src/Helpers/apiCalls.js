bb8d02c4d8d89bdfaca380790fd1af320a4e7217
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-21 15:32:21 -0600

    Return errors to  Snacks component

--- src/Helpers/apiCalls.js
+++ src/Helpers/apiCalls.js
@@ -2,47 +2,65 @@ export const getAllSnacksIds = () => {
   return fetch(`https://api.wegmans.io/products/categories/6942-629-648?api-version=2018-10-18&subscription-key=c455d00cb0f64e238a5282d75921f27e`)
     .then((response) => response.json())
     .then((data) => {
-      let randomSnackIds = [];
-      for (let i = 0; i < 10; i++ ) {
-        const numberFetched = data.products.length;
-        const randomIndex = Math.floor(Math.random() * Math.floor(numberFetched))
-        randomSnackIds.push(data.products[randomIndex].sku)
+      if (data.hasOwnProperty("error")) {
+        console.log(data.error)
+        throw new Error(data)
+      } else {
+        let randomSnackIds = [];
+        for (let i = 0; i < 10; i++ ) {
+          const numberFetched = data.products.length;
+          const randomIndex = Math.floor(Math.random() * Math.floor(numberFetched))
+          randomSnackIds.push(data.products[randomIndex].sku)
+        }
+        return randomSnackIds
       }
-      return randomSnackIds
     })
-};
+    .catch((data) => {return data.error})
+}
 
 export const getSnackDetail = (snackId) => {
   return fetch(`https://api.wegmans.io/products/${snackId}?api-version=2018-10-18&Subscription-Key=c455d00cb0f64e238a5282d75921f27e`)
     .then((response) => response.json())
     .then((data) => {
-      let fetchedDetail = {};
-
-      if (data.descriptions.consumer) {
-        fetchedDetail.name = data.descriptions.consumer;
+      if (data.hasOwnProperty("error")) {
+        console.log(data.error)
+        throw new Error(data)
       } else {
-        fetchedDetail.name = data.name;
+        let fetchedDetail = {};
+
+        if (data.descriptions.consumer) {
+          fetchedDetail.name = data.descriptions.consumer;
+        } else {
+          fetchedDetail.name = data.name;
+        }
+
+        fetchedDetail.brand = data.brand;
+        fetchedDetail.sizeValue = data.size.value;
+        fetchedDetail.sizeUnit = data.size.unitOfMeasure;
+        fetchedDetail.allergens = data.allergens;
+        fetchedDetail.ingredients = data.ingredients;
+        fetchedDetail.organic = data.organic.agency;
+        
+        if (data.tradeIdentifiers.length > 0) {      
+        fetchedDetail.image = data.tradeIdentifiers[0].images[0]
+        }
+
+        return fetchedDetail
       }
-
-      // data.descriptions.consumer ? fetchedDetail.name = data.descriptions.consumer : fetchedDetail.name = data.name;
-
-      fetchedDetail.brand = data.brand;
-      fetchedDetail.sizeValue = data.size.value;
-      fetchedDetail.sizeUnit = data.size.unitOfMeasure;
-      fetchedDetail.allergens = data.allergens;
-      fetchedDetail.ingredients = data.ingredients;
-      fetchedDetail.organic = data.organic.agency;
-      
-      if (data.tradeIdentifiers.length > 0) {      
-      fetchedDetail.image = data.tradeIdentifiers[0].images[0]
-      }
-
-      return fetchedDetail
     })
+    .catch((data) => {return data.error})
 }
 
 export const getSnackPrice = (snackId) => {
   return fetch(`https://api.wegmans.io/products/${snackId}/prices?api-version=2018-10-18&subscription-key=c455d00cb0f64e238a5282d75921f27e`)
     .then((response) => response.json())
-    .then((data) => { return data.stores[0].price })
+    .then((data) => {
+      if (data.hasOwnProperty("error")) {
+        console.log(data.error)
+        throw new Error(data)
+      } else {
+        return data.stores[0].price
+      }
+    })
+    .catch((data) => {return data.error})
 }
