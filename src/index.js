b42fdb59aadf484a05c22e42bb1b57b9fba7be98
Nicole Latifi <>
2020-12-02 20:25:04 -0700

    Create store for redux

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ import './Helpers/Fahkwang/Fahkwang-ExtraLight.ttf'
 import App from './Components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
