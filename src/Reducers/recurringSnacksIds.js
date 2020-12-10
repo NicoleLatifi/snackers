bf4c8bb7aa1f160aecb2b4308dd5edcd502d711c
NicoleLatifi <>
2020-12-10 13:56:06 -0700

    Add remove snack id case

--- src/Reducers/recurringSnacksIds.js
+++ src/Reducers/recurringSnacksIds.js
@@ -1,10 +1,12 @@
 export const recurringSnacksIds = (state = [], action) => {
   switch(action.type) {
       case 'ADD_RECURRING_ID':
-
-        console.log(state)
-
         return ( [...state, action.snackId] )
+      case 'REMOVE_RECURRING-ID':
+        let recurringSnacksIdsCopy = [...state]
+        const index = recurringSnacksIdsCopy.indexOf(action.snackId)
+        recurringSnacksIdsCopy.splice(index, 1)
+        return recurringSnacksIdsCopy
       default: 
         return state
   }
