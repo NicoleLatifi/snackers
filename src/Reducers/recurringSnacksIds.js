c113b05294f174f33e1c1227606f153a55eda51e
NicoleLatifi <>
2020-12-10 22:14:03 -0700

    Add conditional to prevent duplicates

--- src/Reducers/recurringSnacksIds.js
+++ src/Reducers/recurringSnacksIds.js
@@ -1,8 +1,12 @@
 export const recurringSnacksIds = (state = [], action) => {
   switch(action.type) {
       case 'ADD_RECURRING_ID':
-        return ( [...state, action.snackId] )
-      case 'REMOVE_RECURRING-ID':
+        if (!state.includes(action.snackId)) {
+          return ( [...state, action.snackId] )
+        } else {
+          return state
+        }
+      case 'REMOVE_RECURRING_ID':
         let recurringSnacksIdsCopy = [...state]
         const index = recurringSnacksIdsCopy.indexOf(action.snackId)
         recurringSnacksIdsCopy.splice(index, 1)
