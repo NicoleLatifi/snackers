2e3e325f269eeba25dc8ccd3c67aa2f36c2004c6
NicoleLatifi <Nicole.Latifi@gmail.com>
2020-12-17 21:54:38 -0700

    Add actions for allSnacksDetails, allSnacksIds, and totalPrice

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -7,3 +7,23 @@ export const removeRecurringId = (snackId) => ({
   type: 'REMOVE_RECURRING_ID',
   snackId
 })
+
+export const allSnacksDetails = (snackId, recurringStatus) => ({
+  type: 'UPDATE_RECURRING_STATUS',
+  snackId,
+  recurringStatus
+})
+
+export const allSnacksDetails = (snackId, amount) => ({
+  type: 'UPDATE_QUANTITY',
+  snackId,
+  amount
+})
+
+export const setAllSnacks = () => ({
+  type: 'SET_ALL_SNACKS'
+})
+
+export const calculateTotalPrice = () => ({
+  type: 'CALCULATE_TOTAL_PRICE'
+})
