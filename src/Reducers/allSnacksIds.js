45df62f103e740ee8496b60ec8be02d52b279ab2
NicoleLatifi <Nicole.Latifi@gmail.com>
2020-12-17 21:58:27 -0700

    Setup reducer boilerplate

---
+++ src/Reducers/allSnacksIds.js
@@ -0,0 +1,8 @@
+export const allSnacksIds = (state = [], action) => {
+  switch(action.type) {
+      case 'SET_ALL_SNACKS':
+
+      default: 
+        return state
+  }
+}
---
+++ src/Reducers/totalPrice.js
@@ -0,0 +1,8 @@
+export const totalPrice = (state = [], action) => {
+  switch(action.type) {
+      case 'CALCULATE_TOTAL_PRICE':
+
+      default: 
+        return state
+  }
+}
